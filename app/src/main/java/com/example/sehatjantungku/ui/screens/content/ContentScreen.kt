83b3dab23603e5a5027f2ff46d91d21372773a4a
rapsz30 <rafihendryansyah26@gmail.com>
2025-11-27 13:39:35 +0700

    commit

--- app/src/main/java/com/example/sehatjantungku/ui/screens/content/ContentScreen.kt
+++ app/src/main/java/com/example/sehatjantungku/ui/screens/content/ContentScreen.kt
@@ -3,7 +3,6 @@ package com.example.sehatjantungku.ui.screens.content
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
