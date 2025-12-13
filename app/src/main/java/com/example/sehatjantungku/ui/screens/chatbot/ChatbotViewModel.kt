42eef4bd40e4d3db45ffb8783876ffb75d502b36
rapsz30 <rafihendryansyah26@gmail.com>
2025-12-13 22:17:05 +0700

    yahh api key nya public

--- app/src/main/java/com/example/sehatjantungku/ui/screens/chatbot/ChatbotViewModel.kt
+++ app/src/main/java/com/example/sehatjantungku/ui/screens/chatbot/ChatbotViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import com.example.sehatjantungku.BuildConfig // Penting: Import BuildConfig
+import com.example.sehatjantungku.BuildConfig
 
 // Data class pesan chat
 data class ChatMessage(
